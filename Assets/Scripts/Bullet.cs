00095eec423e06d6f89a9a73aafcc39776ac6779
Ренат <syltan.100renrgen@gmail.com>
2022-02-25 00:02:03 +0300

    add bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,39 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private int _damage;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _timeLife;
 
+    private float CurrentTimeLife;
     public int Damage => _damage;
+
+    private void OnValidate()
+    {
+        if (_timeLife <= 0)
+        {
+            _timeLife = 3;
+        }
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        CurrentTimeLife += Time.deltaTime;
+
+        if (CurrentTimeLife >= _timeLife)
+        {
+            CurrentTimeLife = 0;
+            Die();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
 }
